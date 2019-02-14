04285ea6dac5ca2712d9355279b43b409920de0f
juho <krooh@hotmail.com>
2019-02-14 23:51:06 +0200

    up

---
+++ build/precache-manifest.91a57701493ef3bd6913e9d1d91c2bad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "97e7c419d832ca087373",
+    "url": "/static/js/main.97e7c419.chunk.js"
+  },
+  {
+    "revision": "d780f23645c13372e2a8",
+    "url": "/static/js/2.d780f236.chunk.js"
+  },
+  {
+    "revision": "97e7c419d832ca087373",
+    "url": "/static/css/main.700e4ae9.chunk.css"
+  },
+  {
+    "revision": "6178d9405c7f486372346fdca517cfb4",
+    "url": "/index.html"
+  }
+];
