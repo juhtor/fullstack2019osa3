00f4cda9c35a574ba10b4770496b74cf5cb5e80c
juho <krooh@hotmail.com>
2019-02-14 22:10:42 +0200

    frontend build ready for heroku

---
+++ build/precache-manifest.2bc82f2a917c41597ad0f14968fd24c4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "fb76e6432b17ad3ce9eb",
+    "url": "/static/js/main.fb76e643.chunk.js"
+  },
+  {
+    "revision": "d780f23645c13372e2a8",
+    "url": "/static/js/2.d780f236.chunk.js"
+  },
+  {
+    "revision": "fb76e6432b17ad3ce9eb",
+    "url": "/static/css/main.700e4ae9.chunk.css"
+  },
+  {
+    "revision": "5dcdbd0d358ac8b5678af0ee4308175e",
+    "url": "/index.html"
+  }
+];
