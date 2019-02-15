63b00c7126f1464e18a77b8a7e4febb03d1a401b
juho <krooh@hotmail.com>
2019-02-15 06:36:00 +0200

    heroku test

--- index.js
+++ index.js
@@ -92,10 +92,6 @@ app.post('/api/persons', (request, response) => {
 
     response.json(person)
 })
-// app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname + '/build/index.html'));
-// });
-
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
