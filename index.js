a545bf53679c0f60f1a0353e01e380bd97e5964d
juho <krooh@hotmail.com>
2019-02-15 06:13:16 +0200

    up

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors())
 app.use(bodyParser.json())
 morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
-
+app.use(express.static(path.join(__dirname, '/build')));
 const generateId = () => {
     // const maxId =
     //     persons.length > 0
@@ -93,14 +93,19 @@ app.post('/api/persons', (request, response) => {
 
     response.json(person)
 })
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/build/index.html'));
+});
+
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
+
 
 const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: 'unknown endpoint' })
 }
 
 app.use(unknownEndpoint)
-
-const PORT = process.env.PORT || 3001
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-})
