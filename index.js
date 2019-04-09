d9405b89f8d715087f9baf633f60408684a4b3d4
juho <krooh@hotmail.com>
2019-04-09 19:29:58 +0300

    update

--- index.js
+++ index.js
@@ -7,106 +7,187 @@ const Person = require('./models/person')
 const middleware = require('./utils/middleware')
 
 if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config()
+  require('dotenv').config()
 }
 
 app.use(cors())
 app.use(express.static('build'))
 app.use(bodyParser.json())
-morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
+morgan.token('body', function (request, response) { return JSON.stringify(request.body) })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-app.get('/info', (req, res) => {
-    Person
-        .find({})
-        .then(result => {
-            const count = result.length
-            msg = '<p>Puhelinluettelossa ' + count + ' henkilön tiedot</p>'
-                + '<p>' + Date() + '</p>'
-            res.send(msg)
-        })
-        .catch(error => next(error))
+app.get('/info', (reques, response, next) => {
+  Person
+    .find({})
+    .then(result => {
+      const count = result.length
+      const msg = '<p>Puhelinluettelossa ' + count + ' henkilön tiedot</p>'
+        + '<p>' + Date() + '</p>'
+      response.send(msg)
+    })
+    .catch(error => next(error))
 })
 
-app.get('/api/persons', (request, response) => {
-    Person.find({})
-        .then(persons => {
-            response.json(persons.map(person => person.toJSON()))
-        })
-        .catch(error => next(error))
+app.get('/api/persons', (request, response, next) => {
+  Person.find({})
+    .then(persons => {
+      response.json(persons.map(person => person.toJSON()))
+    })
+    .catch(error => next(error))
 })
 
 app.get('/api/persons/:id', (request, response, next) => {
-    Person.findById(request.params.id)
-        .then(person => {
-            if (person) {
-                response.json(person.toJSON())
-            } else {
-                response.status(204).end()
-            }
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person.toJSON())
+      } else {
+        response.status(204).end()
+      }
 
-        })
-        .catch(error => next(error))
+    })
+    .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-    Person.findByIdAndDelete(request.params.id)
-        .then(result => {
-            response.status(204).end()
-        })
-        .catch(error => next(error))
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response, next) => {
-    const body = request.body
-    console.log('name', body.name)
-    if (body.name === undefined || body.number === undefined) {
-        return response.status(400).json({ error: 'name or number missing' })
-    }
+  const body = request.body
+  console.log('name', body.name)
+  if (body.name === undefined || body.number === undefined) {
+    return response.status(400).json({ error: 'name or number missing' })
+  }
 
-    const person = new Person({
-        name: body.name,
-        number: body.number
+  const person = new Person({
+    name: body.name,
+    number: body.number
+  })
+  person.save()
+    .then(savedPerson => {
+      console.log('saved', savedPerson)
+      response.json(savedPerson.toJSON())
     })
-    person.save()
-        .then(savedPerson => {
-            console.log('saved', savedPerson)
-            response.json(savedPerson.toJSON())
-        })
-        .catch(error => next(error))
+    .catch(error => next(error))
 
-    // Person.find({ name: body.name })
-    //     .then(foundPerson => {
-    //         console.log(foundPerson, 'found person to check if name already exists')
-    //         if (foundPerson.length > 0) {
-    //             return response.status(400).json({ error: 'name already in the list' })
-    //         }
+  // Person.find({ name: body.name })
+  //     .then(foundPerson => {
+  //         console.log(foundPerson, 'found person to check if name already exists')
+  //         if (foundPerson.length > 0) {
+  //             return response.status(400).json({ error: 'name already in the list' })
+  //         }
 
-    //         const person = new Person({
-    //             name: body.name,
-    //             number: body.number
-    //         })
+  //         const person = new Person({
+  //             name: body.name,
+  //             number: body.number
+  //         })
 
-    //         person.save()
-    //             .then(savedPerson => {
-    //                 console.log('saved', savedPerson)
-    //                 response.json(savedPerson.toJSON())
-    //             })
+  //         person.save()
+  //             .then(savedPerson => {
+  //                 console.log('saved', savedPerson)
+  //                 response.json(savedPerson.toJSON())
+  //             })
 
-    //     })
+  //     })
 
 })
-app.put('/api/persons/:id', (request, response) => {
-    const body = request.body
-    const person = {
-        name: body.name,
-        number: body.number
-    }
-    Person.findByIdAndUpdate(request.params.id, person, { new: true })
-        .then(updatedPerson => {
-            response.json(updatedPerson.toJSON())
-        })
-        .catch(error => { next(error) })
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+  const person = {
+    name: body.name,
+    number: body.number
+  }
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson.toJSON())
+    })
+    .catch(error => { next(error) })
+})
+
+app.get('/persons', (request, response, next) => {
+  Person.find({})
+    .then(persons => {
+      response.json(persons.map(person => person.toJSON()))
+    })
+    .catch(error => next(error))
+})
+
+app.get('/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person.toJSON())
+      } else {
+        response.status(204).end()
+      }
+
+    })
+    .catch(error => next(error))
+})
+
+app.delete('/persons/:id', (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+app.post('/persons', (request, response, next) => {
+  const body = request.body
+  console.log('name', body.name)
+  if (body.name === undefined || body.number === undefined) {
+    return response.status(400).json({ error: 'name or number missing' })
+  }
+
+  const person = new Person({
+    name: body.name,
+    number: body.number
+  })
+  person.save()
+    .then(savedPerson => {
+      console.log('saved', savedPerson)
+      response.json(savedPerson.toJSON())
+    })
+    .catch(error => next(error))
+
+  // Person.find({ name: body.name })
+  //     .then(foundPerson => {
+  //         console.log(foundPerson, 'found person to check if name already exists')
+  //         if (foundPerson.length > 0) {
+  //             return response.status(400).json({ error: 'name already in the list' })
+  //         }
+
+  //         const person = new Person({
+  //             name: body.name,
+  //             number: body.number
+  //         })
+
+  //         person.save()
+  //             .then(savedPerson => {
+  //                 console.log('saved', savedPerson)
+  //                 response.json(savedPerson.toJSON())
+  //             })
+
+  //     })
+
+})
+app.put('/persons/:id', (request, response, next) => {
+  const body = request.body
+  const person = {
+    name: body.name,
+    number: body.number
+  }
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson.toJSON())
+    })
+    .catch(error => { next(error) })
 })
 app.use(middleware.unknownEndpoint)
 
@@ -115,7 +196,7 @@ app.use(middleware.errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
 
 
--- models/person.js
+++ models/person.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose')
-uniqueValidator = require('mongoose-unique-validator')
+const uniqueValidator = require('mongoose-unique-validator')
 
 if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config()
+  require('dotenv').config()
 }
 
 const url = process.env.MONGODB_URI
@@ -10,33 +10,33 @@ const url = process.env.MONGODB_URI
 console.log('commecting to', url)
 
 mongoose.connect(url, { useNewUrlParser: true })
-    .then(result => {
-        console.log('connected to MongoDB')
-    })
-    .catch((error) => {
-        console.log('error connection to MongoDB:', error.message)
-    })
+  .then(result => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connection to MongoDB:', error.message)
+  })
 
 const personSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        unique: true,
-        minlength: 3,
-        required: true
-    },
-    number: {
-        type: String,
-        minlength: 8,
-        required: true
-    }
+  name: {
+    type: String,
+    unique: true,
+    minlength: 3,
+    required: true
+  },
+  number: {
+    type: String,
+    minlength: 8,
+    required: true
+  }
 })
 
 personSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 
 module.exports = mongoose.model('Person', personSchema)
